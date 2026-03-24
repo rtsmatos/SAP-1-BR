cc8e2f5cb214f5146a27a6d0c34db01485157378
Ricardo Matos <43769547+rtsmatos@users.noreply.github.com>
2026-03-24 10:01:33 -0300

    Update ram.v

--- ram.v
+++ ram.v
@@ -1,14 +1,14 @@
 module ram (
-    input clock,          // pulso de 1 ciclo (mesmo usado no SAP)
+    input clock,     
 	 input we,
-    input ram_in,             // sinal de controle da UC (modo RUN)
-    input ram_out,            // controle de leitura
-    input prog_run,           // 0 = modo PROG, 1 = modo RUN
-    input [3:0] mar_to_ram,   // endereço (MAR)
+    input ram_in, //sinal de escrita no modo RUN
+    input ram_out,  
+    input prog_run,  // 0 = modo PROG, 1 = modo RUN
+	input [3:0] mar_to_ram,  // endereço recebido do MAR
     input [7:0] bus_in,       // dado vindo do barramento
     input [7:0] prog_data, // switches do usuário (modo PROG)
     output [7:0] bus_out,
-	 output [16*8 -1:0] ram_to_vga  //expondo todas as linhas da RAM
+	output [127:0] ram_to_vga  //recebe todo o conteudo da RAM em apenas 1 vetor de 128 bits
 );
 
     reg [7:0] memory[0:15];
@@ -29,12 +29,11 @@ module ram (
 
     assign bus_out = ram_out ? memory[mar_to_ram] : 8'bzzzzzzzz;
 	 
-	 //Achatar o conteudo da ram para enviar para o modulo vga
-	 genvar i;
-    generate
-        for (i = 0; i < 16; i = i + 1) begin : memory_output
-            assign ram_to_vga[(i+1)*8-1 -: 8] = memory[i];
-        end
-    endgenerate
+	always @(*) begin
+	    integer j;
+	    for (j = 0; j < 16; j = j + 1) begin
+	        ram_to_vga[j*8 +: 8] = memory[j];
+	    end
+	end
 	 
 endmodule 
